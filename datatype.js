1c66fe5c8d0f198a2d68cd4ab1e40d510ee52ada
hetvi <dhaneshahetvi.com>
2026-01-28 13:34:21 +0530

    [DH][added two files]

---
+++ datatype.js
@@ -0,0 +1,157 @@
+//two type of data types
+//1.primitve
+//Number
+//BigInt
+//String
+//Boolean
+//Symbol
+//null
+//undefined
+
+//2.Non primitive
+//Array
+//object
+//function
+
+//let num = 1;
+//let num2 = 2;
+
+//console.log("num",num,num2);
+
+//let name = "hetvi";
+//let name2 = "dhanesha";
+
+//console.log("names : ",name2,name);
+
+//boolean
+
+//let isTrue = true;
+//let isFalse = father;
+
+//console.log("boolean :", isTrue)
+
+//undefined
+
+//let a;
+//console.log("undefined :",a)
+
+//let BigInt = 1234567806543233n;
+//console.log(BigInt);
+
+//let student = {
+   // name : "hetvi",
+   // roll : 65,
+        //address:{
+         //   city:"jnd",
+       // }
+//}
+//console.log(student)
+
+//Array
+
+// let a = [1,2,3,4,5,6,7,8,9];
+// let student = {
+
+// }
+// let arrayOfstud = [{},{}]
+
+//console.log(a);
+
+//funcation
+
+// function greet(){
+//     console.log("this is function");
+// }
+// greet();
+
+// function showValues(value){
+//     console.log( "this is value:",value);
+//     if (value == 20){
+//         console.log("number--------");
+//     }
+// }
+// showValues();
+// showValues(19);
+// showValues([1,2,3,4,5]);
+// showValues();
+
+// const add = (num1 = 5,num2 = 5) => {
+//     return num1 + num2
+//     console.log(Object)
+// }
+
+
+// console.log('this is arrow fun. :', add())
+
+// const sub = (num1 = 10,num2 = 5) => {
+//     return num1 - num2
+//     console.log(Object)
+// }
+// console.log('this is arrow fun. :', sub())
+
+// const div = (num1 = 10,num2 = 5) => {
+//     return num1 / num2
+//     console.log(Object)
+// }
+// console.log('this is arrow fun. :', div())
+
+// const multi = (num1 = 10,num2 = 5) => {
+//     return num1 * num2
+//     console.log(Object)
+// }
+// console.log('this is arrow fun. :', multi())
+
+
+
+//Addition
+
+// function sum(num1,num2){
+//     console.log("sum is :",num1 + num2);
+// }
+// console.log(5+10);
+
+//Sub
+
+// function sub(num1,num2){
+//     console.log("sub is :",num1 - num2);
+// }
+// console.log(15-10);
+
+//div
+
+// function div(num1,num2){
+//     console.log("div is :",num1 / num2);
+// }
+// console.log(15/10);
+
+//multi
+
+// function multi(num1,num2){
+//     console.log("multi is :",num1 * num2);
+// }
+// console.log(15 * 10);
+
+// let value = 10;
+// console.log('DEFAULT VALUE:',value);
+
+// value += 5
+// console.log(value)
+
+// value -= 5
+// console.log(value)
+
+// value /= 5
+// console.log(value)
+
+// value *= 5
+// console.log(value)
+
+// 
+
+let value = 10;
+let num = "10"
+
+console.log("value == num :", value == num);
+console.log("value === num :", value === num);
+console.log("value != num :", value != num);
+
---
+++ variable.js
@@ -0,0 +1,14 @@
+//var a = 10;
+//console.log(a);
+
+let a = 20;
+
+if (true){
+    let a= 10;
+}
+console.log(a);
+
+//const
+
+const a = 3.14;
+console.log(a);
